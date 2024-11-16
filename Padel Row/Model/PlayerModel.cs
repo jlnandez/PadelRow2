790fd7a393c51d516cd05986bd642a1303cce425
jlnandez <54343551+jlnandez@users.noreply.github.com>
2024-11-15 18:15:14 -0800

    Se agrega para que cambie INOTIFY en Score y cambios visuales

--- Padel Row/Model/PlayerModel.cs
+++ Padel Row/Model/PlayerModel.cs
@@ -1,15 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
 
 namespace Padel_Row.Model
 {
-    public class PlayerModel
+    public class PlayerModel : INotifyPropertyChanged
     {
-        public string Id { get; set; }
+        private int _score;
         public string Name { get; set; }
-        public int Score { get; set; } = 0; // Puntaje inicializado en 0
+        public int Score
+        {
+            get => _score;
+            set
+            {
+                if (_score != value)
+                {
+                    _score = value;
+                    OnPropertyChanged(nameof(Score)); // Notifica que la propiedad ha cambiado
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
